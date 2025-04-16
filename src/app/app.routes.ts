78408cbf121a55b89c9f31ab1c79c90112d77e37
petertechy <petertechy01@gmail.com>
2025-04-16 09:13:19 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { ContactAddComponent } from './components/contact-add/contact-add.component';
+import { ContactEditComponent } from './components/contact-edit/contact-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ContactListComponent },
+  { path: 'add', component: ContactAddComponent },
+  { path: 'edit/:id', component: ContactEditComponent },
+];
