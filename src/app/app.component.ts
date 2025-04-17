bb29126e3c0bc08f4b00fb6c1ca6ecc5fc1372ed
petertechy <petertechy01@gmail.com>
2025-04-17 20:17:26 +0100

    Updated app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastService } from './services/toast.service'; // Import ToastService
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'contact-app';
+
+  constructor(public toastService: ToastService) {}
+
+  get toastMessage(): string {
+    return this.toastService.toastMessage;
+  }
 }
