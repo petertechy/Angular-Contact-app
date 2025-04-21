7eedaeac399cb4ac639d6d77be4c90e375eb16c4
petertechy <petertechy01@gmail.com>
2025-04-21 23:59:03 +0100

    Updated app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
 import { ContactAddComponent } from './components/contact-add/contact-add.component';
 import { ContactEditComponent } from './components/contact-edit/contact-edit.component';
+import { FavoriteContactsComponent } from './components/favorite-contacts/favorite-contacts.component';
 
 export const routes: Routes = [
   { path: '', component: ContactListComponent },
   { path: 'add', component: ContactAddComponent },
   { path: 'edit/:id', component: ContactEditComponent },
+  { path: 'favorites', component: FavoriteContactsComponent }, // ✅ NEW
 ];
+
